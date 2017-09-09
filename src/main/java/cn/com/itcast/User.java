2626fbef635ffcd4fca0b66c3ac80082c0271bfe
黄小龙 <huangxiaolong@tinck.local>
2017-09-09 14:27:22 +0800

    second commit

---
+++ src/main/java/cn/com/itcast/User.java
@@ -0,0 +1,10 @@
+package cn.com.itcast;
+
+/**
+ * Created by huangxiaolong on 2017/9/8.
+ */
+public class User {
+    public void add(){
+        System.out.println("add add ......");
+    }
+}
---
+++ src/test/java/cn/com/itcast/UserTest.java
@@ -0,0 +1,18 @@
+package cn.com.itcast;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by huangxiaolong on 2017/9/8.
+ */
+public class UserTest {
+    @Test
+    public void getU(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
+        User user = (User) context.getBean("user");
+        System.out.println(user);
+        user.add();
+    }
+}
